c96ac595142ff9ea9a35e8890547e24172f22d5a
rdhox <admin@rdhox.io>
2020-04-19 18:34:13 +0200

    first commit

---
+++ main/src/electron.js
@@ -0,0 +1,34 @@
+"use strict";
+exports.__esModule = true;
+var electron_1 = require("electron");
+var path = require("path");
+var isDev = require("electron-is-dev");
+require("electron-reload");
+var mainWindow;
+function createWindow() {
+    mainWindow = new electron_1.BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    mainWindow.loadURL(isDev
+        ? 'http://localhost:3000'
+        : "file://" + path.join(__dirname, '../build/index.html'));
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+electron_1.app
+    .on('ready', createWindow)
+    .on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        electron_1.app.quit();
+    }
+})
+    .on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
---
+++ main/src/electron.ts
@@ -0,0 +1,39 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+import * as isDev from 'electron-is-dev';
+import 'electron-reload';
+
+let mainWindow: (BrowserWindow | null);
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`,
+  );
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app
+  .on('ready', createWindow)
+  .on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+  .on('activate', () => {
+    if (mainWindow === null) {
+      createWindow()
+    }
+  });
